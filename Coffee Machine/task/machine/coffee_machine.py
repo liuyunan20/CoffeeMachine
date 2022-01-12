da358ed279ac125b6bee16d22b8c7020dad0b076
liuyunan <liuyunan.20@gmail.com>
2022-01-12 00:36:39 -0800

    calculate how many cups the machine could make

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,9 +1,17 @@
+print("Write how many ml of water the coffee machine has:")
+water = int(input())
+print("Write how many ml of milk the coffee machine has:")
+milk = int(input())
+print("Write how many grams of coffee beans the coffee machine has:")
+coffee_beans = int(input())
 print("Write how many cups of coffee you will need:")
-cups = int(input())
-water = 200 * cups
-milk = 50 * cups
-coffee_beans = 15 * cups
-print(f'''For {cups} cups of coffee you will need:
-{water} ml of water
-{milk} ml of milk
-{coffee_beans} g of coffee beans''')
+need_cups = int(input())
+
+cups = min(water // 200, milk // 50, coffee_beans // 15)
+
+if cups == need_cups:
+    print("Yes, I can make that amount of coffee")
+elif cups > need_cups:
+    print(f"Yes, I can make that amount of coffee (and even {cups - need_cups} more than that)")
+else:
+    print(f"No, I can make only {cups} cups of coffee")
